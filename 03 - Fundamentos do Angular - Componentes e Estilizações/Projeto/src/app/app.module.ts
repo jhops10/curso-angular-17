57bec6c72d92253b62ed2eb0d1d459ca10cd7f02
João Paulo <joaowais@gmail.com>
2024-01-29 20:40:43 -0300

    aula 04 finalizada

--- 03 - Fundamentos do Angular - Componentes e Estilizações/Projeto/src/app/app.module.ts
+++ 03 - Fundamentos do Angular - Componentes e Estilizações/Projeto/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardsModule } from './cards/cards.module';
+import { CardButtonRoxoComponent } from './card-button-roxo/card-button-roxo.component';
 
 @NgModule({
   declarations: [AppComponent],
--- 03 - Fundamentos do Angular - Componentes e Estilizações/Projeto/src/app/cards/cards.module.ts
+++ 03 - Fundamentos do Angular - Componentes e Estilizações/Projeto/src/app/cards/cards.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from '../card/card.component';
 import { CardButtonComponent } from '../card-button/card-button.component';
+import { CardButtonCancelComponent } from '../card-button-cancel/card-button-cancel.component';
+import { CardRoxoComponent } from '../card-roxo/card-roxo.component';
+import { CardButtonRoxoComponent } from '../card-button-roxo/card-button-roxo.component';
 
 @NgModule({
-  declarations: [CardComponent, CardButtonComponent],
+  declarations: [
+    CardComponent,
+    CardRoxoComponent,
+    CardButtonComponent,
+    CardButtonCancelComponent,
+    CardButtonRoxoComponent,
+  ],
   imports: [CommonModule],
-  exports: [CardComponent, CardButtonComponent],
+  exports: [CardComponent, CardButtonComponent, CardRoxoComponent],
 })
 export class CardsModule {}
