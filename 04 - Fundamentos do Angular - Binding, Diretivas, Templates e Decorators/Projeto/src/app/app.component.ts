f4a8ebc61afefde062ca31370f4034b4087039f4
João Paulo <joaowais@gmail.com>
2024-02-01 20:48:57 -0300

    aula 01 finalizada

--- 04 - Fundamentos do Angular - Binding, Diretivas, Templates e Decorators/Projeto/src/app/app.component.ts
+++ 04 - Fundamentos do Angular - Binding, Diretivas, Templates e Decorators/Projeto/src/app/app.component.ts
@@ -3,8 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'Projeto';
+  inputText: string = 'Texto Inicial Alterado';
+  inputType: string = 'text';
+  isDisabled: boolean = true;
+
+  enableInput() {
+    this.isDisabled = false;
+  }
+
+  disableInput() {
+    this.isDisabled = true;
+  }
+
+  changeTypeToText() {
+    this.inputType = 'text';
+  }
+
+  changeTypeToPassword() {
+    this.inputType = 'password';
+  }
+
+  logInputText() {
+    console.log(this.inputText);
+  }
 }
