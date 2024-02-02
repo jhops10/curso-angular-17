7a27740b8fcd9eae5cb6389abfd650a647009147
João Paulo <joaowais@gmail.com>
2024-02-02 17:50:01 -0300

    aula 02 finalizada

--- 04 - Fundamentos do Angular - Binding, Diretivas, Templates e Decorators/Projeto/src/app/app.component.ts
+++ 04 - Fundamentos do Angular - Binding, Diretivas, Templates e Decorators/Projeto/src/app/app.component.ts
@@ -6,27 +6,40 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  inputText: string = 'Texto Inicial Alterado';
-  inputType: string = 'text';
-  isDisabled: boolean = true;
+  // inputText: string = 'Texto Inicial Alterado';
+  // inputType: string = 'text';
+  // isDisabled: boolean = true;
 
-  enableInput() {
-    this.isDisabled = false;
-  }
+  // enableInput() {
+  //   this.isDisabled = false;
+  // }
 
-  disableInput() {
-    this.isDisabled = true;
-  }
+  // disableInput() {
+  //   this.isDisabled = true;
+  // }
 
-  changeTypeToText() {
-    this.inputType = 'text';
-  }
+  // changeTypeToText() {
+  //   this.inputType = 'text';
+  // }
 
-  changeTypeToPassword() {
-    this.inputType = 'password';
-  }
+  // changeTypeToPassword() {
+  //   this.inputType = 'password';
+  // }
 
-  logInputText() {
-    console.log(this.inputText);
+  // logInputText() {
+  //   console.log(this.inputText);
+  // }
+
+  // handleInputKeyUp(event: KeyboardEvent) {
+  //   const currentText = (event.target as HTMLInputElement).value;
+  //   console.log(currentText);
+  // }
+
+  buttonTitle: string = 'Título do Botão';
+  buttonDisable: boolean = false;
+
+  onButtonClick() {
+    this.buttonTitle = 'Titulo Alteradoooo';
+    this.buttonDisable = !this.buttonDisable;
   }
 }
