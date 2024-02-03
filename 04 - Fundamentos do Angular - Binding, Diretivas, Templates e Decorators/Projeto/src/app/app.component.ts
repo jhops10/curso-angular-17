f48e7883d4558b60f48681e33b0f0ab22f22298d
João Paulo <joaowais@gmail.com>
2024-02-03 16:08:01 -0300

    aula 03 finalizada

--- 04 - Fundamentos do Angular - Binding, Diretivas, Templates e Decorators/Projeto/src/app/app.component.ts
+++ 04 - Fundamentos do Angular - Binding, Diretivas, Templates e Decorators/Projeto/src/app/app.component.ts
@@ -9,37 +9,47 @@ export class AppComponent {
   // inputText: string = 'Texto Inicial Alterado';
   // inputType: string = 'text';
   // isDisabled: boolean = true;
-
   // enableInput() {
   //   this.isDisabled = false;
   // }
-
   // disableInput() {
   //   this.isDisabled = true;
   // }
-
   // changeTypeToText() {
   //   this.inputType = 'text';
   // }
-
   // changeTypeToPassword() {
   //   this.inputType = 'password';
   // }
-
   // logInputText() {
   //   console.log(this.inputText);
   // }
-
   // handleInputKeyUp(event: KeyboardEvent) {
   //   const currentText = (event.target as HTMLInputElement).value;
   //   console.log(currentText);
   // }
+  // buttonTitle: string = 'Título do Botão';
+  // buttonDisable: boolean = false;
+  // onButtonClick() {
+  //   this.buttonTitle = 'Titulo Alteradoooo';
+  //   this.buttonDisable = !this.buttonDisable;
+  // }
+  // widthButton1: string = '110px';
+  // backgroundColorButton1: string = 'gray';
+  // widthButton2: string = '150';
+  // stylesObj = {
+  //   width: '125px',
+  //   backgroundColor: 'white',
+  //   padding: '10px',
+  // };
+  // updateStyleObj() {
+  //   this.stylesObj = {
+  //     backgroundColor: 'aqua',
+  //     width: '200px',
+  //     padding: '15px',
+  //   };
+  // }
 
-  buttonTitle: string = 'Título do Botão';
-  buttonDisable: boolean = false;
-
-  onButtonClick() {
-    this.buttonTitle = 'Titulo Alteradoooo';
-    this.buttonDisable = !this.buttonDisable;
-  }
+  isGreen: boolean = true;
+  isRedButton: boolean = true;
 }
