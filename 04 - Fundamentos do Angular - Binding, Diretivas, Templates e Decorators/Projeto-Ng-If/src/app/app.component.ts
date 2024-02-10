72aedc1aadb2f10ce5480da16ad02607746f9271
João Paulo <joaowais@gmail.com>
2024-02-10 17:05:46 -0300

    aula 01 finalizada

--- 04 - Fundamentos do Angular - Binding, Diretivas, Templates e Decorators/Projeto-Ng-If/src/app/app.component.ts
+++ 04 - Fundamentos do Angular - Binding, Diretivas, Templates e Decorators/Projeto-Ng-If/src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'Projeto-Ng-If';
+  minhaProp = true;
+
+  toggleProp() {
+    this.minhaProp = !this.minhaProp;
+  }
 }
