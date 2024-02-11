5462eb6290d73bc0eb8e0f00ef81a1b9ac62bf72
João Paulo <joaowais@gmail.com>
2024-02-11 13:36:31 -0300

    aula 01 finalizada

--- 04 - Fundamentos do Angular - Binding, Diretivas, Templates e Decorators/Projeto-Pipes/src/app/app.component.ts
+++ 04 - Fundamentos do Angular - Binding, Diretivas, Templates e Decorators/Projeto-Pipes/src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'Projeto-Pipes';
+  pessoa = {
+    name: 'João',
+    status: 1,
+  };
+
+  pessoa2 = {
+    name: 'Matheus',
+    status: 2,
+  };
+
+  pessoa3 = {
+    name: 'Bella',
+    status: 3,
+  };
 }
--- 04 - Fundamentos do Angular - Binding, Diretivas, Templates e Decorators/Projeto-Pipes/src/app/app.module.ts
+++ 04 - Fundamentos do Angular - Binding, Diretivas, Templates e Decorators/Projeto-Pipes/src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StatusClassPipe } from './pipes/status-class.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, StatusClassPipe],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
