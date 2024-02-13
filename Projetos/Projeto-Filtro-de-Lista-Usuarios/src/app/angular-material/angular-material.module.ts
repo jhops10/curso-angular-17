098e4e61f8e4d9d628095da8fe17b25914545066
João Paulo <joaowais@gmail.com>
2024-02-13 18:24:38 -0300

    aula 02 finalizada

--- Projetos/Projeto-Filtro-de-Lista-Usuarios/src/app/angular-material/angular-material.module.ts
+++ Projetos/Projeto-Filtro-de-Lista-Usuarios/src/app/angular-material/angular-material.module.ts
@@ -3,9 +3,38 @@ import { NgModule } from '@angular/core';
 //Angular Material
 import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
-  imports: [MatListModule, MatDividerModule],
-  exports: [MatListModule, MatDividerModule],
+  imports: [
+    MatListModule,
+    MatDividerModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTableModule,
+  ],
+  exports: [
+    MatListModule,
+    MatDividerModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTableModule,
+  ],
+  providers: [provideNativeDateAdapter()],
 })
 export class AngularMaterialModule {}
