cbec4c1624664f886ecfac374dddd7bb3df3a8b0
João Paulo <joaowais@gmail.com>
2024-02-14 19:34:59 -0300

    criacao do pipe para valores vazios

--- Projetos/Projeto-Filtro-de-Lista-Usuarios/src/app/pipes/pipes.module.ts
+++ Projetos/Projeto-Filtro-de-Lista-Usuarios/src/app/pipes/pipes.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { PhonePipe } from './phone.pipe';
 import { AdressPipe } from './adress.pipe';
 import { StatusPipe } from './status.pipe';
+import { DashIfEmptyPipe } from './dash-if-empty.pipe';
 
 @NgModule({
-  declarations: [PhonePipe, AdressPipe, StatusPipe],
-  exports: [PhonePipe, AdressPipe, StatusPipe],
+  declarations: [PhonePipe, AdressPipe, StatusPipe, DashIfEmptyPipe],
+  exports: [PhonePipe, AdressPipe, StatusPipe, DashIfEmptyPipe],
 })
 export class PipesModule {}
