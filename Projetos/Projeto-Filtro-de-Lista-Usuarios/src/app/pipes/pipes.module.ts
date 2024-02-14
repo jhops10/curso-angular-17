911b9afc1ba1edf96c9926c293f28bd74bf1120b
João Paulo <joaowais@gmail.com>
2024-02-14 13:06:56 -0300

    criacao do pipe status

--- Projetos/Projeto-Filtro-de-Lista-Usuarios/src/app/pipes/pipes.module.ts
+++ Projetos/Projeto-Filtro-de-Lista-Usuarios/src/app/pipes/pipes.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { PhonePipe } from './phone.pipe';
 import { AdressPipe } from './adress.pipe';
+import { StatusPipe } from './status.pipe';
 
 @NgModule({
-  declarations: [PhonePipe, AdressPipe],
-  exports: [PhonePipe, AdressPipe],
+  declarations: [PhonePipe, AdressPipe, StatusPipe],
+  exports: [PhonePipe, AdressPipe, StatusPipe],
 })
 export class PipesModule {}
