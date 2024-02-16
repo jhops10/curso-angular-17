2e8a2ba0be6527e744e1a8d546337a5f9b2a2fc5
João Paulo <joaowais@gmail.com>
2024-02-16 17:03:58 -0300

    externalizacao da logica de filtro e projeto finalizado

--- Projetos/Projeto-Filtro-de-Lista-Usuarios/src/app/app.component.ts
+++ Projetos/Projeto-Filtro-de-Lista-Usuarios/src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { IUser } from './interfaces/user/user.interface';
 import { UsersList } from './data/users-list';
 import { IFilterOptions } from './interfaces/filter-options.interface';
-
-import { isWithinInterval } from 'date-fns';
+import { IUser } from './interfaces/user/user.interface';
+import { filterUsersList } from './utils/filter-users-list';
 
 @Component({
   selector: 'app-root',
@@ -20,7 +19,7 @@ export class AppComponent implements OnInit {
     setTimeout(() => {
       this.usersList = UsersList;
       this.usersListFiltered = this.usersList;
-    }, 3);
+    }, 3000);
   }
 
   onUserSelected(user: IUser) {
@@ -30,75 +29,6 @@ export class AppComponent implements OnInit {
 
   onFilter(filterOptions: IFilterOptions) {
     console.log(filterOptions);
-    this.usersListFiltered = this.filterUsersList(
-      filterOptions,
-      this.usersList
-    );
-  }
-
-  filterUsersList(filterOptions: IFilterOptions, usersList: IUser[]): IUser[] {
-    let filteredList: IUser[] = [];
-
-    filteredList = this.filterUsersListByName(filterOptions.name, usersList);
-    filteredList = this.filterUsersListByStatus(
-      filterOptions.status,
-      filteredList
-    );
-    filteredList = this.filterUsersListByDate(
-      filterOptions.startDate,
-      filterOptions.endDate,
-      filteredList
-    );
-    return filteredList;
-  }
-
-  filterUsersListByName(name: string | undefined, usersList: IUser[]): IUser[] {
-    const NAME_NOT_TYPPED = name === undefined;
-
-    if (NAME_NOT_TYPPED) {
-      return usersList;
-    }
-
-    const filteredList = usersList.filter((user) =>
-      user.nome.toLowerCase().includes(name.toLowerCase())
-    );
-
-    return filteredList;
-  }
-
-  filterUsersListByStatus(
-    status: boolean | undefined,
-    usersList: IUser[]
-  ): IUser[] {
-    const STATUS_NOT_SELECTED = status === undefined;
-
-    if (STATUS_NOT_SELECTED) {
-      return usersList;
-    }
-
-    const filteredList = usersList.filter((user) => user.ativo === status);
-
-    return filteredList;
-  }
-
-  filterUsersListByDate(
-    startDate: Date | undefined,
-    endDate: Date | undefined,
-    usersList: IUser[]
-  ): IUser[] {
-    const DATES_NOT_SELECTED = startDate === undefined || endDate === undefined;
-
-    if (DATES_NOT_SELECTED) {
-      return usersList;
-    }
-
-    const listFiltered = usersList.filter((user) =>
-      isWithinInterval(new Date(user.dataCadastro), {
-        start: startDate,
-        end: endDate,
-      })
-    );
-
-    return listFiltered;
+    this.usersListFiltered = filterUsersList(filterOptions, this.usersList);
   }
 }
