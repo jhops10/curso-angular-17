7590363099e06598a17d5d0740ac1ba2b32eb6a5
João Paulo <joaowais@gmail.com>
2024-02-16 16:53:30 -0300

    criacao de filtro por data

--- Projetos/Projeto-Filtro-de-Lista-Usuarios/src/app/app.component.ts
+++ Projetos/Projeto-Filtro-de-Lista-Usuarios/src/app/app.component.ts
@@ -3,6 +3,8 @@ import { IUser } from './interfaces/user/user.interface';
 import { UsersList } from './data/users-list';
 import { IFilterOptions } from './interfaces/filter-options.interface';
 
+import { isWithinInterval } from 'date-fns';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -42,6 +44,11 @@ export class AppComponent implements OnInit {
       filterOptions.status,
       filteredList
     );
+    filteredList = this.filterUsersListByDate(
+      filterOptions.startDate,
+      filterOptions.endDate,
+      filteredList
+    );
     return filteredList;
   }
 
@@ -72,5 +79,26 @@ export class AppComponent implements OnInit {
     const filteredList = usersList.filter((user) => user.ativo === status);
 
     return filteredList;
+  }
+
+  filterUsersListByDate(
+    startDate: Date | undefined,
+    endDate: Date | undefined,
+    usersList: IUser[]
+  ): IUser[] {
+    const DATES_NOT_SELECTED = startDate === undefined || endDate === undefined;
+
+    if (DATES_NOT_SELECTED) {
+      return usersList;
+    }
+
+    const listFiltered = usersList.filter((user) =>
+      isWithinInterval(new Date(user.dataCadastro), {
+        start: startDate,
+        end: endDate,
+      })
+    );
+
+    return listFiltered;
   }
 }
