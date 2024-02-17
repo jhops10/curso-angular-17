97e96062a44435ca4ad9396214751aa0ac7bc356
João Paulo <joaowais@gmail.com>
2024-02-16 23:11:03 -0300

    aula 01 finalizada

--- 05 - Fundamentos do Angular - Diretivas, Templates e Decorators Dinâmicos/Projeto/src/app/app.component.ts
+++ 05 - Fundamentos do Angular - Diretivas, Templates e Decorators Dinâmicos/Projeto/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'Projeto';
+  textValue = '';
+
+  constructor(private _cdRef: ChangeDetectorRef) {}
+
+  changeDetection() {
+    this._cdRef.detectChanges();
+  }
 }
