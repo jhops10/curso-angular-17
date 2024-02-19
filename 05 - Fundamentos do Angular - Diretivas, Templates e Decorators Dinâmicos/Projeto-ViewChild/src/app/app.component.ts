fada6c8a77e190ddb37c50a0e41984fcba8bc594
João Paulo <joaowais@gmail.com>
2024-02-19 17:28:41 -0300

    aula 01 finalizada

--- 05 - Fundamentos do Angular - Diretivas, Templates e Decorators Dinâmicos/Projeto-ViewChild/src/app/app.component.ts
+++ 05 - Fundamentos do Angular - Diretivas, Templates e Decorators Dinâmicos/Projeto-ViewChild/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'Projeto-ViewChild';
+  //Exemplo 1 (Aula 01)
+  // @ViewChild('meuInput') meuInputEl!: ElementRef<HTMLInputElement>;
+  // @ViewChild('minhaDiv') minhaDivEl!: ElementRef<HTMLDivElement>;
+  // updateInputText() {
+  //   this.meuInputEl.nativeElement.value = 'Texto Atualizado!';
+  //   console.log(this.meuInputEl);
+  // }
+  // focus() {
+  //   this.meuInputEl.nativeElement.focus();
+  // }
+  // changeContent() {
+  //   this.minhaDivEl.nativeElement.textContent = 'Olá, Mundo';
+  // }
 }
