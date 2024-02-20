914c7d0ebd5d7150fe463b799efebf5c5bc35634
João Paulo <joaowais@gmail.com>
2024-02-20 15:34:18 -0300

    aula 01 finalizada

--- 05 - Fundamentos do Angular - Diretivas, Templates e Decorators Dinâmicos/Projeto-Diretivas/src/app/app.module.ts
+++ 05 - Fundamentos do Angular - Diretivas, Templates e Decorators Dinâmicos/Projeto-Diretivas/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SemDiretivaComponent } from './sem-diretiva/sem-diretiva.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SemDiretivaComponent
   ],
   imports: [
     BrowserModule,
