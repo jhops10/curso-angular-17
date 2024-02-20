0e29f6f4401456dc031da5fb6f9dfa9f6532e2f5
João Paulo <joaowais@gmail.com>
2024-02-20 16:39:02 -0300

    aula 03 finalizada

--- 05 - Fundamentos do Angular - Diretivas, Templates e Decorators Dinâmicos/Projeto-Diretivas/src/app/app.module.ts
+++ 05 - Fundamentos do Angular - Diretivas, Templates e Decorators Dinâmicos/Projeto-Diretivas/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { SemDiretivaComponent } from './sem-diretiva/sem-diretiva.component';
 import { ComDiretivaComponent } from './com-diretiva/com-diretiva.component';
 import { HighlightDirective } from './directives/highlight.directive';
+import { DisabledDirective } from './directives/disabled.directive';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { HighlightDirective } from './directives/highlight.directive';
     SemDiretivaComponent,
     ComDiretivaComponent,
     HighlightDirective,
+    DisabledDirective,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
---
+++ 05 - Fundamentos do Angular - Diretivas, Templates e Decorators Dinâmicos/Projeto-Diretivas/src/app/directives/disabled.directive.ts
@@ -0,0 +1,10 @@
+import { Directive, HostBinding } from '@angular/core';
+
+@Directive({
+  selector: '[appDisabled]',
+})
+export class DisabledDirective {
+  @HostBinding('disabled') propDisabled = false;
+
+  constructor() {}
+}
