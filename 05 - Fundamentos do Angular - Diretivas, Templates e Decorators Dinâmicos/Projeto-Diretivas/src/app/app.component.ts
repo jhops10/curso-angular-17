b6792db4b2d8ba93fd931c531f72a9eb808a8ea1
João Paulo <joaowais@gmail.com>
2024-02-21 17:25:14 -0300

    aula 08 finalizada

--- 05 - Fundamentos do Angular - Diretivas, Templates e Decorators Dinâmicos/Projeto-Diretivas/src/app/app.component.ts
+++ 05 - Fundamentos do Angular - Diretivas, Templates e Decorators Dinâmicos/Projeto-Diretivas/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'Projeto-Diretivas';
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild('minhaDiv') divEl!: ElementRef<HTMLDivElement>;
+
+  constructor(private _elRef: ElementRef) {}
+
+  ngOnInit(): void {
+    console.log(this._elRef);
+    const divEl = this._elRef.nativeElement.querySelector(
+      '#minha-outra-div'
+    ) as HTMLDivElement;
+    console.log(divEl);
+  }
+
+  ngAfterViewInit(): void {
+    this.divEl.nativeElement.style.backgroundColor = 'orange';
+    this.divEl.nativeElement.textContent = 'Sou uma div!';
+    this.divEl.nativeElement.classList.add('minha-classe');
+  }
 }
