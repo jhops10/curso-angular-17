94cf076c08e0c0c5596e7e1e344be63737aed785
João Paulo <joaowais@gmail.com>
2024-02-22 21:49:41 -0300

    titulo do card dinamico

--- Projetos/02-Projeto-Cards/src/app/components/card/card.component.ts
+++ Projetos/02-Projeto-Cards/src/app/components/card/card.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrl: './card.component.scss'
+  styleUrl: './card.component.scss',
 })
 export class CardComponent {
-
+  @Input({ required: true }) cardTitle: string = '';
+  @Input() balance: string = '';
 }
