93cec538c9b575d33fcd86ec89807933f96463e8
João Paulo <joaowais@gmail.com>
2024-02-24 20:08:59 -0300

    refatoração do item-list

--- Projetos/02-Projeto-Cards/src/app/components/item-list/item-list.component.ts
+++ Projetos/02-Projeto-Cards/src/app/components/item-list/item-list.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-item-list',
   templateUrl: './item-list.component.html',
-  styleUrl: './item-list.component.scss'
+  styleUrl: './item-list.component.scss',
 })
 export class ItemListComponent {
-
+  @Input({ required: true }) text: string = '';
+  @Input({ required: true }) value: string = '';
 }
