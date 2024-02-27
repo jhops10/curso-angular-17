7c85e76ebb56c6a202b18b938b2d8ebd877b775d
João Paulo <joaowais@gmail.com>
2024-02-27 16:38:54 -0300

    aula 01 finalizada

--- 06 - Fundamentos do Angular - Projeção de Conteúdo e Estilização com Diretivas/Projeto-Seletor-Elemento/src/app/components/card/directives/card-text-description.directive.ts
+++ 06 - Fundamentos do Angular - Projeção de Conteúdo e Estilização com Diretivas/Projeto-Seletor-Elemento/src/app/components/card/directives/card-text-description.directive.ts
@@ -1,9 +1,11 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: 'app-card-text-description',
   host: { class: 'ca-u-card-text-description' },
 })
 export class CardTextDescriptionDirective {
-  constructor() {}
+  @Input()
+  @HostBinding('style.color')
+  color: string = '';
 }
