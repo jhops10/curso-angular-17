d236bba505a9a469d69ff6d5f1686539deb3f047
João Paulo <joaowais@gmail.com>
2024-02-28 15:41:37 -0300

    adicionado duas diretivas (header e content)

--- 06 - Fundamentos do Angular - Projeção de Conteúdo e Estilização com Diretivas/Projeto/src/app/app.module.ts
+++ 06 - Fundamentos do Angular - Projeção de Conteúdo e Estilização com Diretivas/Projeto/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
 import { CompTextoComponent } from './comp-texto/comp-texto.component';
+import { CardHeaderDirective } from './directives/card-header.directive';
+import { CardContentDirective } from './directives/card-content.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
-    CompTextoComponent
+    CompTextoComponent,
+    CardHeaderDirective,
+    CardContentDirective
   ],
   imports: [
     BrowserModule,
