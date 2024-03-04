835eeb7c0444c3e79152293f5d1cdbaa06108149
João Paulo <joaowais@gmail.com>
2024-03-04 19:01:01 -0300

    aula 04 finalizada

--- 07 - Fundamentos do Angular - Tópicos Avançados Projeção de Conteúdo/Projeto/src/app/app.component.ts
+++ 07 - Fundamentos do Angular - Tópicos Avançados Projeção de Conteúdo/Projeto/src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './ng-template-outlet/ex4.html',
+  templateUrl: './ng-if/ex1.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  condition: boolean = true;
+  condition: boolean = false;
 }
