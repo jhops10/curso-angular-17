2a2836cdc9f01167689085ffa4f4b63de3c75c4c
João Paulo <joaowais@gmail.com>
2024-03-04 19:35:26 -0300

    aula 06 finalizada

--- 07 - Fundamentos do Angular - Tópicos Avançados Projeção de Conteúdo/Projeto/src/app/card/card.component.ts
+++ 07 - Fundamentos do Angular - Tópicos Avançados Projeção de Conteúdo/Projeto/src/app/card/card.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentInit,
+  Component,
+  ContentChild,
+  OnInit,
+  TemplateRef,
+} from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
 })
-export class CardComponent {
+export class CardComponent implements OnInit, AfterContentInit {
   condition: boolean = false;
+  @ContentChild('meuTemplate') content!: TemplateRef<any>;
+
+  ngOnInit(): void {
+    console.log('OnInit', this.content);
+  }
+
+  ngAfterContentInit(): void {
+    console.log('AfterContentInit', this.content);
+  }
 }
