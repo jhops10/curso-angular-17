ed29ad60ab84899df8cabf1caf92b1aad1bec7a7
João Paulo <joaowais@gmail.com>
2024-03-05 19:43:06 -0300

    Aula 01 finalizada

--- 08 - Fundamentos do Angular - Pipes/Projeto/src/app/app.component.ts
+++ 08 - Fundamentos do Angular - Pipes/Projeto/src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'Projeto';
+  pessoa = {
+    nome: 'João Paulo',
+    idade: 33,
+  };
 }
