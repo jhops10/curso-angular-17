de6295f9f432f1681cabfcb14bf867f486bdb02a
João Paulo <joaowais@gmail.com>
2024-03-05 22:24:58 -0300

    aula 01 finalizada

--- 08 - Fundamentos do Angular - Pipes/Projeto-Pipe-Date/src/app/app.component.ts
+++ 08 - Fundamentos do Angular - Pipes/Projeto-Pipe-Date/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'Projeto-Pipe-Date';
+export class AppComponent implements OnInit {
+  minhaData: string = '2023-10-21T21:00:00.000Z';
+  minhaDataObj: Date = new Date('2023-10-21T21:00:00.000Z');
+
+  ngOnInit(): void {
+    console.log('Data Convertida para Região', new Date(this.minhaData));
+    console.log('Data UTC-0', new Date(this.minhaData).toUTCString());
+    console.log('Timestemp', new Date(this.minhaData).getTime());
+    console.log('', this.minhaDataObj);
+  }
 }
