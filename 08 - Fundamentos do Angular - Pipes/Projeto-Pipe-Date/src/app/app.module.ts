15eba6f29351e4fd12323474868bedc9d4a70edb
João Paulo <joaowais@gmail.com>
2024-03-07 20:58:37 -0300

    aula 03 finalizada

--- 08 - Fundamentos do Angular - Pipes/Projeto-Pipe-Date/src/app/app.module.ts
+++ 08 - Fundamentos do Angular - Pipes/Projeto-Pipe-Date/src/app/app.module.ts
@@ -1,18 +1,32 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { DATE_PIPE_DEFAULT_OPTIONS } from '@angular/common';
+
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localePt, 'pt-BR');
+
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt-BR',
+    },
+    {
+      provide: DATE_PIPE_DEFAULT_OPTIONS,
+      useValue: {
+        dateFormat: "'Data: 'dd/MM/YYYY",
+        timezone: '+0000',
+      },
+    },
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
