dc4facffb5fd859c547769dbc565a7fb753b3710
João Paulo <joaowais@gmail.com>
2024-04-08 15:55:04 -0300

    aula 04 finalizada

--- 09 - Fundamentos do Angular - Template Driven Forms/Projeto-ElementoForm-Diretiva-NgForm/src/app/elements/form/form.component.ts
+++ 09 - Fundamentos do Angular - Template Driven Forms/Projeto-ElementoForm-Diretiva-NgForm/src/app/elements/form/form.component.ts
@@ -4,12 +4,39 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  styleUrls: ['./form.component.scss'],
 })
 export class FormComponent implements AfterViewInit {
+  // Aula 03
   @ViewChild('meuForm') form!: NgForm;
-
   ngAfterViewInit() {
     console.log(this.form);
+  }
+
+  // Aula 04
+  inputText: string = 'Texto Inicial';
+  textarea: string = 'Descrição inicial';
+  select: string = '1';
+  radioButton: string = '1';
+  checkbox: boolean = true;
+
+  onSubmit(form: NgForm) {
+    console.log('onSubmit', form.value);
+    // form.value['nome'] = 'Texto Alterado';
+    // console.log(this.inputText);
+  }
+
+  onReset(form: NgForm) {
+    form.reset();
+    console.log('onReset', form.value);
+    console.log(this.inputText);
+    console.log(this.textarea);
+    console.log(this.select);
+    console.log(this.radioButton);
+    console.log(this.checkbox);
+
+    setTimeout(() => {
+      console.log(form.value);
+    }, 1000);
   }
 }
