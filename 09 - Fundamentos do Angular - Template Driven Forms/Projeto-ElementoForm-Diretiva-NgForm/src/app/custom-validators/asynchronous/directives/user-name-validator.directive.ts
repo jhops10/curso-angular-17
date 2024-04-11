02bbc5c731c47f73c65117342bfe2bd53f7b2cff
João Paulo <joaowais@gmail.com>
2024-04-10 21:18:08 -0300

    aula 11 finalizada

--- 09 - Fundamentos do Angular - Template Driven Forms/Projeto-ElementoForm-Diretiva-NgForm/src/app/custom-validators/asynchronous/directives/user-name-validator.directive.ts
+++ 09 - Fundamentos do Angular - Template Driven Forms/Projeto-ElementoForm-Diretiva-NgForm/src/app/custom-validators/asynchronous/directives/user-name-validator.directive.ts
@@ -1,10 +1,43 @@
-import { Directive } from '@angular/core';
+import { UsersService } from './../service/users.service';
+import { Directive, forwardRef } from '@angular/core';
+import {
+  AbstractControl,
+  AsyncValidator,
+  NG_ASYNC_VALIDATORS,
+  ValidationErrors,
+} from '@angular/forms';
+import { Observable, delay, map, of } from 'rxjs';
 
 @Directive({
-  selector: '[appUserNameValidator]'
+  selector: '[appUserNameValidator]',
+  providers: [
+    {
+      provide: NG_ASYNC_VALIDATORS,
+      useExisting: forwardRef(() => UserNameValidatorDirective),
+      multi: true,
+    },
+  ],
 })
-export class UserNameValidatorDirective {
+export class UserNameValidatorDirective implements AsyncValidator {
+  constructor(private readonly _usersService: UsersService) {}
 
-  constructor() { }
+  validate(
+    control: AbstractControl<any, any>
+  ): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
+    if (!control.dirty) {
+      return of(null);
+    }
+    return this._usersService.getUsers().pipe(
+      delay(3000),
+      map((users) => {
+        const foundUser = users.find((user) => user.name === control.value);
 
+        if (foundUser) {
+          return null;
+        }
+
+        return { invalidUserName: true };
+      })
+    );
+  }
 }
