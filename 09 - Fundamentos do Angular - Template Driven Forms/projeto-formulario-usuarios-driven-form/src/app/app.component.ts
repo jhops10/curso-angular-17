29c555dc058c38783d8c070312e47c21520ce6e6
João Paulo <joaowais@gmail.com>
2024-05-03 11:14:57 -0300

    Criação do UsersService

--- 09 - Fundamentos do Angular - Template Driven Forms/projeto-formulario-usuarios-driven-form/src/app/app.component.ts
+++ 09 - Fundamentos do Angular - Template Driven Forms/projeto-formulario-usuarios-driven-form/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from './services/users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'projeto-formulario-usuarios-driven-form';
+export class AppComponent implements OnInit {
+  usersList: any = [];
+  constructor(private readonly _usersService: UsersService) {}
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this._usersService.getUsers().subscribe((usersListResponse) => {
+      this.usersList = usersListResponse;
+    });
+  }
 }
