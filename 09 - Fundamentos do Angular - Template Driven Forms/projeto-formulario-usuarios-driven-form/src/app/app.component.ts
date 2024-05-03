f56400d82e2bd3eba497e7dd261e4c7521da9ba1
João Paulo <joaowais@gmail.com>
2024-05-03 11:24:56 -0300

    Criação do genresService

--- 09 - Fundamentos do Angular - Template Driven Forms/projeto-formulario-usuarios-driven-form/src/app/app.component.ts
+++ 09 - Fundamentos do Angular - Template Driven Forms/projeto-formulario-usuarios-driven-form/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from './services/users.service';
+import { GenresService } from './services/genres.service';
 
 @Component({
   selector: 'app-root',
@@ -8,15 +9,26 @@ import { UsersService } from './services/users.service';
 })
 export class AppComponent implements OnInit {
   usersList: any = [];
-  constructor(private readonly _usersService: UsersService) {}
+  genresList: any = [];
+  constructor(
+    private readonly _usersService: UsersService,
+    private readonly _genresService: GenresService
+  ) {}
 
   ngOnInit(): void {
     this.getUsers();
+    this.getGenres();
   }
 
-  getUsers() {
+  private getUsers() {
     this._usersService.getUsers().subscribe((usersListResponse) => {
       this.usersList = usersListResponse;
+    });
+  }
+
+  private getGenres() {
+    this._genresService.getGenres().subscribe((genresListResponse) => {
+      this.genresList = genresListResponse;
     });
   }
 }
