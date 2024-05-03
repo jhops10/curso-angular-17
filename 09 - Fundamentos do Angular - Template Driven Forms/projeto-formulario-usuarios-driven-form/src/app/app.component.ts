d68bc7f90d73bdc839e956d4f93fefada33bb185
João Paulo <joaowais@gmail.com>
2024-05-03 16:52:13 -0300

    Criação do brazilianStatesService

--- 09 - Fundamentos do Angular - Template Driven Forms/projeto-formulario-usuarios-driven-form/src/app/app.component.ts
+++ 09 - Fundamentos do Angular - Template Driven Forms/projeto-formulario-usuarios-driven-form/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from './services/users.service';
 import { GenresService } from './services/genres.service';
+import { BrazilianStatesService } from './services/brazilian-states.service';
 
 @Component({
   selector: 'app-root',
@@ -10,25 +11,37 @@ import { GenresService } from './services/genres.service';
 export class AppComponent implements OnInit {
   usersList: any = [];
   genresList: any = [];
+  statesList: any = [];
   constructor(
     private readonly _usersService: UsersService,
-    private readonly _genresService: GenresService
+    private readonly _genresService: GenresService,
+    private readonly _brazilianStatesService: BrazilianStatesService
   ) {}
 
   ngOnInit(): void {
     this.getUsers();
     this.getGenres();
+    this.getStates();
   }
 
   private getUsers() {
     this._usersService.getUsers().subscribe((usersListResponse) => {
       this.usersList = usersListResponse;
+      console.log(this.usersList);
     });
   }
 
   private getGenres() {
     this._genresService.getGenres().subscribe((genresListResponse) => {
       this.genresList = genresListResponse;
+      console.log(this.genresList);
+    });
+  }
+
+  private getStates() {
+    this._brazilianStatesService.getStates().subscribe((statesListResponse) => {
+      this.statesList = statesListResponse;
+      console.log(this.statesList);
     });
   }
 }
