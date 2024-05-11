4928e8a032ac2a492208887d64571cbdf211e099
João Paulo <joaowais@gmail.com>
2024-05-11 19:32:20 -0300

    passagem dos inputs para user-form

--- 09 - Fundamentos do Angular - Template Driven Forms/projeto-formulario-usuarios-driven-form/src/app/components/user-form/user-form.component.ts
+++ 09 - Fundamentos do Angular - Template Driven Forms/projeto-formulario-usuarios-driven-form/src/app/components/user-form/user-form.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import { GenreListResponse } from '../../types/genre-list-response';
+import { StateListResponse } from '../../types/state-list-response';
+import { IUser } from '../../interfaces/user/user.interface';
 
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
-  styleUrl: './user-form.component.scss'
+  styleUrl: './user-form.component.scss',
 })
-export class UserFormComponent {
+export class UserFormComponent implements OnInit, OnChanges {
+  @Input() genresList: GenreListResponse = [];
+  @Input() stateList: StateListResponse = [];
+  @Input() userSelected: IUser = {} as IUser;
 
+  constructor() {}
+
+  ngOnInit(): void {
+    // console.log(' <- NgOnInit -> ');
+    // console.log(this.genresList);
+    // console.log(this.stateList);
+    // console.log(this.userSelected);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(' <- NgOnChange -> ');
+    console.log(this.genresList);
+    console.log(this.stateList);
+    console.log(this.userSelected);
+  }
 }
