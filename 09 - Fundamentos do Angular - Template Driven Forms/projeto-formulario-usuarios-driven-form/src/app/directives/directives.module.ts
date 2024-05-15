390926f252c89078ad1c45e361a6c4301832108d
João Paulo <joaowais@gmail.com>
2024-05-14 23:16:55 -0300

    criação do campo senha

--- 09 - Fundamentos do Angular - Template Driven Forms/projeto-formulario-usuarios-driven-form/src/app/directives/directives.module.ts
+++ 09 - Fundamentos do Angular - Template Driven Forms/projeto-formulario-usuarios-driven-form/src/app/directives/directives.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { EmailPatternValidatorDirective } from './email-pattern-validator.directive';
 import { CredentialsValidatorDirective } from './credentials-validator.directive';
+import { PasswordStrengthValidatorDirective } from './password-strength-validator.directive';
 
 @NgModule({
-  declarations: [EmailPatternValidatorDirective, CredentialsValidatorDirective],
-  exports: [EmailPatternValidatorDirective, CredentialsValidatorDirective],
+  declarations: [
+    EmailPatternValidatorDirective,
+    CredentialsValidatorDirective,
+    PasswordStrengthValidatorDirective,
+  ],
+  exports: [
+    EmailPatternValidatorDirective,
+    CredentialsValidatorDirective,
+    PasswordStrengthValidatorDirective,
+  ],
 })
 export class DirectivesModule {}
