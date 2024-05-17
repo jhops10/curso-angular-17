99afad1e54eadde0d39c19090808aa1a94468025
João Paulo <joaowais@gmail.com>
2024-05-16 23:36:20 -0300

    criação do campo de estado

--- 09 - Fundamentos do Angular - Template Driven Forms/projeto-formulario-usuarios-driven-form/src/app/angular-material.module.ts
+++ 09 - Fundamentos do Angular - Template Driven Forms/projeto-formulario-usuarios-driven-form/src/app/angular-material.module.ts
@@ -8,6 +8,7 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   providers: [],
@@ -19,6 +20,7 @@ import { MatButtonModule } from '@angular/material/button';
     MatDatepickerModule,
     MatNativeDateModule,
     MatButtonModule,
+    MatSelectModule,
   ],
   exports: [
     MatCardModule,
@@ -28,6 +30,7 @@ import { MatButtonModule } from '@angular/material/button';
     MatDatepickerModule,
     MatNativeDateModule,
     MatButtonModule,
+    MatSelectModule,
   ],
 })
 export class AngularMaterialModule {}
